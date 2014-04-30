8e96c1da1b10d180a5fd15a80e21f6d407ba74b6
Clay Miller <clay@smockle.com>
2014-04-29 20:29:14 -0500

    Add dashboard. Enable CRUD for all models.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :item_templates
 
   get 'home/index'
+  get 'home/dashboard'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
